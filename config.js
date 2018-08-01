f4232951600cab67789cced3365de6f3f9c77c77
dao <dao@jing-MacBook-Pro.local>
2018-08-01 13:54:06 +0800

    Initial Commit

---
+++ config.js
@@ -0,0 +1,13 @@
+window.option = {
+  title: 'cookie',
+  logo:'',
+  banner:'https://cdn.dribbble.com/users/245692/screenshots/4896298/selfie-dribbble-crop.jpg',
+  bannerTitle:'hello world',
+  footer:'2018 cookie\'s Blog',
+  owner:'cookieHe',
+  repo: 'blog',
+  oauth: {
+    client_id: '58ed27b34ea65650dd61',
+    client_secret: '9bf4cc15ba562d627e9ed939e31ec4ef5680010a',
+  },
+}
